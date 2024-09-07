38aeaf05ebb3c48596cb9bc068ea4f60eac515e2
Mel-Rocha <rochamel73@gmail.com>
2024-09-07 14:48:20 -0300

    grafico de calorias e de macros

--- apps/diet_macors_sheets.py
+++ apps/diet_macors_sheets.py
@@ -19,6 +19,7 @@ def setup_diet_macros_sheets_dashboard(app):
     dash_app.layout = html.Div([
         html.H1("Dashboard Diet Macros Sheets"),
         dcc.Graph(id='macros-data-graph'),
+        dcc.Graph(id='calories-data-graph'),
         dcc.Interval(
             id='interval-component',
             interval=10 * 60 * 1000,  # Atualiza a cada 10 minutos
@@ -27,7 +28,8 @@ def setup_diet_macros_sheets_dashboard(app):
     ])
 
     @dash_app.callback(
-        Output('macros-data-graph', 'figure'),
+        [Output('macros-data-graph', 'figure'),
+         Output('calories-data-graph', 'figure')],
         Input('interval-component', 'n_intervals')
     )
     def update_macros_data_graph(n_intervals):
@@ -36,8 +38,24 @@ def setup_diet_macros_sheets_dashboard(app):
             response.raise_for_status()
             data = response.json()
             df = pd.DataFrame(data)
-            fig = px.bar(df, x='id', y=['cho', 'ptn', 'fat', 'kcal'], title="Dados de Macronutrientes", labels={"value": "Valor", "id": "ID"})
-            fig.update_layout(barmode='group')
-            return fig
+
+            # Gráfico de colunas agrupadas para macronutrientes
+            macros_fig = px.bar(df,
+                               x='id',
+                               y=['cho', 'ptn', 'fat'],
+                               title="Dados de Macronutrientes",
+                               labels={"value": "Valor", "id": "ID"})
+            macros_fig.update_layout(barmode='group')
+
+            # Gráfico de linhas para calorias
+            calories_fig = px.line(df,
+                                  x='id',
+                                  y='kcal',
+                                  title="Calorias",
+                                  labels={"kcal": "Calorias", "id": "ID"})
+
+            return macros_fig, calories_fig
         except Exception as e:
-            return px.bar(title=f"Error: {str(e)}")
+            error_fig = px.bar(title=f"Error: {str(e)}")
+            return error_fig, error_fig
+
