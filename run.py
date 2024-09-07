82a828c36f07ad67979e1e60195035f61eef155a
Mel-Rocha <rochamel73@gmail.com>
2024-09-07 19:36:27 -0300

    só precisamos de gráficos para isso

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 # run.py
 
 from flask import Flask, render_template_string
-from apps.taco import setup_taco_dashboard
 from apps.diet_macors_sheets import setup_diet_macros_sheets_dashboard
 from apps.meal_macros_sheets import setup_meal_macros_sheets_dashboard
 from apps.daiily_records import setup_daily_records_dashboard
@@ -9,7 +8,6 @@ from apps.daiily_records import setup_daily_records_dashboard
 app = Flask(__name__)
 
 # Configurações dos Dashboards
-setup_taco_dashboard(app)
 setup_diet_macros_sheets_dashboard(app)
 setup_meal_macros_sheets_dashboard(app)
 setup_daily_records_dashboard(app)
@@ -21,7 +19,7 @@ def home():
         <head><title>API Dashboard</title></head>
         <body>
             <h1>API Dashboard</h1>
-            <button onclick="window.location.href='/dashboard/taco/'">Taco Data Dashboard</button><br><br>
+            <button onclick="window.location.href='/dashboard/daily-records/'">Registros Diários Data Dashboard</button><br><br>
             <button onclick="window.location.href='/dashboard/macros/'">Diet Macros Sheets Dashboard</button><br><br>
             <button onclick="window.location.href='/dashboard/meal-macros/'">Meal Macros Sheets Dashboard</button><br><br>
         </body>
